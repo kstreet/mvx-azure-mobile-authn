cb3ebd642209eac1b66fcb3cfbf13a804092673f
Kerry Street <kerrycstreet@gmail.com>
2013-08-24 16:13:44 -0700

    Target Plugin's core to Profile 136 to get Task

---
+++ BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone/AuthenticationProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone
+{
+    public class AuthenticationProvider : IAuthenticationProvider
+    {
+        public AuthenticationResult Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings)
+        {
+            // for now this is the synch slow network call that the plugin's cosumer will
+            // wrap in a Task so it can await it.  "LoginService" for example.
+            // maybe this part of the platform-specific plugin INCLUDES that
+            // service and Mvx Viewmodel?
+        }
+    }
+}
--- BeingTheWorst.MvxPlugins.AzureMobileAuthN/IAuthenticationProvider.cs
+++ BeingTheWorst.MvxPlugins.AzureMobileAuthN/IAuthenticationProvider.cs
@@ -1,10 +1,11 @@
-﻿namespace BeingTheWorst.MvxPlugins.AzureMobileAuthN
+﻿using System.Threading.Tasks;
+
+namespace BeingTheWorst.MvxPlugins.AzureMobileAuthN
 {
     public interface IAuthenticationProvider
     {
         // can't use Task<AuthenticationResult> and support WP 7.5/7.1 because
         // MS Task/Async NuGet support is restricted to Windows-only BCL license!
-        // TODO: Should I use some xplat Action callback here instead or doesn't matter?
-        AuthenticationResult Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings);
+        Task<AuthenticationResult> Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings);
     }
 }
