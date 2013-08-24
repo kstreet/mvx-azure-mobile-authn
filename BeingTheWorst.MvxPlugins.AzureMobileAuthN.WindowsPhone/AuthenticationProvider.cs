ff39b08ef43374309ed912797a25c2a88fcd69df
Kerry Street <kerrycstreet@gmail.com>
2013-08-24 16:48:05 -0700

    implement IAuthenticationProvider for WP8

--- BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone/AuthenticationProvider.cs
+++ BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone/AuthenticationProvider.cs
@@ -1,16 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone
 {
     public class AuthenticationProvider : IAuthenticationProvider
     {
-        public Task<AuthenticationResult> Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings)
+        public async Task<AuthenticationResult> Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings)
         {
-            throw new NotImplementedException();
+            // setup the real Azure Mobile Services client provided by MS SDK
+            var mobileSvcsClient = new
+                MobileServiceClient(providerSettings.UrlToAuthenticationProvider,
+                                    providerSettings.ApplicationIdKeyFromProvider);
+
+            // default to MS account if whatever passed in is a no go
+            // TODO: See Azure Mobile GitHub to see how they check for invalid Enum values
+            var azmobProvider = MobileServiceAuthenticationProvider.MicrosoftAccount;
+
+            // see which Azure Mobile Services Authentication Provider they want to use
+            switch (providerType)
+            {
+                case AuthNProviderType.Google:
+                    azmobProvider = MobileServiceAuthenticationProvider.Google;
+                    break;
+                case AuthNProviderType.Facebook:
+                    azmobProvider = MobileServiceAuthenticationProvider.Facebook;
+                    break;
+                case AuthNProviderType.Twitter:
+                    azmobProvider = MobileServiceAuthenticationProvider.Twitter;
+                    break;
+                case AuthNProviderType.Microsoft:
+                    azmobProvider = MobileServiceAuthenticationProvider.MicrosoftAccount;
+                    break;
+                default:
+                    break;
+            }
+
+            await mobileSvcsClient.LoginAsync(azmobProvider);
+
+            var authResult = new AuthenticationResult
+            {
+                ProviderName = mobileSvcsClient.ToString(),
+                IdentityString = mobileSvcsClient.CurrentUser.UserId,
+                MobileServicesUserToken = mobileSvcsClient.CurrentUser.MobileServiceAuthenticationToken
+            };
+
+            return authResult;
         }
     }
 }
