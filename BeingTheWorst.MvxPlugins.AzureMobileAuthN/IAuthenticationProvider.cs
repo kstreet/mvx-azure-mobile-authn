394730cc618e5b46606d6dd925656f601591dc56
Kerry Street <kerrycstreet@gmail.com>
2013-08-24 14:20:11 -0700

    Action callbacks?

--- BeingTheWorst.MvxPlugins.AzureMobileAuthN/IAuthenticationProvider.cs
+++ BeingTheWorst.MvxPlugins.AzureMobileAuthN/IAuthenticationProvider.cs
@@ -3,7 +3,8 @@
     public interface IAuthenticationProvider
     {
         // can't use Task<AuthenticationResult> and support WP 7.5/7.1 because
-        // MS Task/Async NuGet support is restricted to Windows-only BCL license! 
+        // MS Task/Async NuGet support is restricted to Windows-only BCL license!
+        // TODO: Should I use some xplat Action callback here instead or doesn't matter?
         AuthenticationResult Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings);
     }
 }
