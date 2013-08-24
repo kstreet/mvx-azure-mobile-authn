c1db58cd6f1f168270a1ed65d06f8d83525584ce
Kerry Street <kerrycstreet@gmail.com>
2013-08-24 16:51:08 -0700

    add Mvx plugin boilerplate for WP8

---
+++ BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using Cirrious.CrossCore;
+using Cirrious.CrossCore.Plugins;
+
+namespace BeingTheWorst.MvxPlugins.AzureMobileAuthN.WindowsPhone
+{
+    // Plugin boilerplate code for the Implementation-side
+    public class Plugin : IMvxPlugin
+    {
+        // you can be certain that this
+        // Plugin Load method will only be called once
+        public void Load()
+        {
+            // can use the same IoC methods here that
+            // you use in Setup and App
+
+            // whenever someone asks for an IAuthenticationProvider on THIS platform
+            // give them the AuthenticationProvider implemented in this DLL
+
+            Mvx.RegisterType<IAuthenticationProvider, AuthenticationProvider>();
+        }
+    }
+}
