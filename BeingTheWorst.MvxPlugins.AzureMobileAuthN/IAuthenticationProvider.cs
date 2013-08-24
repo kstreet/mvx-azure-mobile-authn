8c4f3a2dafe62c7f62953129c7b8140c157d40b9
Kerry Street <kerrycstreet@gmail.com>
2013-08-24 14:18:01 -0700

    note about Async/Task BCL MS license issue

--- BeingTheWorst.MvxPlugins.AzureMobileAuthN/IAuthenticationProvider.cs
+++ BeingTheWorst.MvxPlugins.AzureMobileAuthN/IAuthenticationProvider.cs
@@ -2,6 +2,8 @@
 {
     public interface IAuthenticationProvider
     {
+        // can't use Task<AuthenticationResult> and support WP 7.5/7.1 because
+        // MS Task/Async NuGet support is restricted to Windows-only BCL license! 
         AuthenticationResult Authenticate(AuthNProviderType providerType, AuthNProviderSettings providerSettings);
     }
 }
